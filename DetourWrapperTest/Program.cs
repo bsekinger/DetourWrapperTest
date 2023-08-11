87db7862944257bd07b657e7e003e2b4f7d2dfc6
bsekinger <bsekinger@gmail.com>
2023-08-11 14:11:50 -0400

    replaced DllPath attribute with LibraryImport attribute

--- DetourWrapperTest/Program.cs
+++ DetourWrapperTest/Program.cs
@@ -4,48 +4,47 @@
 using System;
 using System.Runtime.InteropServices;
 
-public class DetourWrapper
+public unsafe partial class DetourWrapper
 {
     const string DllPath = @"D:\My Documents\Repos\DetourWrapper\x64\Debug\DetourWrapper.dll";
+    [LibraryImport(DllPath), UnmanagedCallConv]
+    //[DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+    public static partial void* allocDetour();
 
-    [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
-    public static unsafe extern void* allocDetour();
+    [LibraryImport(DllPath), UnmanagedCallConv]
+    //[DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+    public static partial void freeDetour(void* detourPtr);
 
-    [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
-    public static unsafe extern void freeDetour(void* detourPtr);
-
-    [DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
-    public static unsafe extern uint load(void* detourPtr, [MarshalAs(UnmanagedType.LPStr)] string filePath);
+    [LibraryImport(DllPath), UnmanagedCallConv]
+    //[DllImport(DllPath, CallingConvention = CallingConvention.Cdecl)]
+    public static partial uint load(void* detourPtr, [MarshalAs(UnmanagedType.LPStr)] string filePath);
 
     // Add future functions...
 }
 
 class Program
 {
-    static void Main()
+    static unsafe void Main()
     {
-        unsafe
+        void* detourPtr = DetourWrapper.allocDetour();
+        if (detourPtr != null)
         {
-            void* detourPtr = DetourWrapper.allocDetour();
-            if (detourPtr != null)
+            string filePath = @"D:\My Documents\Repos\DetourWrapperTest\DetourWrapperTest\Meshes\87.bin";
+            uint result = DetourWrapper.load(detourPtr, filePath);
+            if (result != 0)
             {
-                string filePath = @"D:\My Documents\Repos\DetourWrapperTest\DetourWrapperTest\Meshes\87.bin";
-                uint result = DetourWrapper.load(detourPtr, filePath);
-                if (result != 0)
-                {
-                    Console.WriteLine("Load successful!");
-                }
-                else
-                {
-                    Console.WriteLine("Load failed!");
-                }
-
-                DetourWrapper.freeDetour(detourPtr);
+                Console.WriteLine("Load successful!");
             }
             else
             {
-                Console.WriteLine("Detour allocation failed!");
+                Console.WriteLine("Load failed!");
             }
+
+            DetourWrapper.freeDetour(detourPtr);
+        }
+        else
+        {
+            Console.WriteLine("Detour allocation failed!");
         }
     }
 }
